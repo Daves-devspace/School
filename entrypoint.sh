8c1126185def249b2215df12b847a3cd1284c905
David Maina <dmdeveloper255@gmail.com>
2025-01-22 22:56:24 +0300

    render2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-# Wait for PostgreSQL to be ready
-echo "Waiting for PostgreSQL..."
-while ! nc -z -v -w30 $DB_HOST $DB_PORT; do
-  echo "Waiting for database connection..."
+
+# Wait for database and Redis to be ready
+echo "Waiting for the database..."
+while ! nc -z $DB_HOST $DB_PORT; do
   sleep 1
 done
-echo "PostgreSQL is up!"
+echo "Database is ready!"
 
-# Wait for Redis to be ready
-echo "Waiting for Redis..."
-while ! nc -z -v -w30 $REDIS_HOST 6379; do
-  echo "Waiting for Redis connection..."
-  sleep 1
-done
-echo "Redis is up!"
+# Run database migrations
+echo "Applying database migrations..."
+python manage.py migrate --noinput
 
-# Run Django app
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+# Start Gunicorn
+echo "Starting Gunicorn..."
 exec "$@"
