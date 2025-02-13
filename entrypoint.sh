992de78a013df90d45555883e6c0042b84b68412
David Maina <dmdeveloper255@gmail.com>
2025-02-13 12:00:42 +0300

    vercel1

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,37 @@
+##!/bin/bash
+#
+## Function to log with timestamp
+#log() {
+#  echo "$(date +'%Y-%m-%d %H:%M:%S') - $1"
+#}
+#
+## Wait for the database to be ready
+#log "Waiting for the database..."
+#while ! nc -z $DB_HOST $DB_PORT; do
+#  log "Waiting for the database..."
+#  sleep 1
+#done
+#log "Database is ready!"
+#
+## Wait for Redis to be ready (ensure REDIS_PORT is set to 6379)
+#log "Waiting for Redis..."
+#while ! nc -z $REDIS_HOST 6379; do
+#  log "Waiting for Redis..."
+#  sleep 1
+#done
+#log "Redis is ready!"
+#
+## Run database migrations
+#log "Applying database migrations..."
+#python manage.py migrate --noinput
+#
+## Collect static files
+#log "Collecting static files..."
+#python manage.py collectstatic --noinput
+#
+## Start Gunicorn
+#log "Starting Gunicorn..."
+#exec "$@"
 #!/bin/bash
 
 # Function to log with timestamp
@@ -13,15 +47,7 @@ while ! nc -z $DB_HOST $DB_PORT; do
 done
 log "Database is ready!"
 
-# Wait for Redis to be ready (ensure REDIS_PORT is set to 6379)
-log "Waiting for Redis..."
-while ! nc -z $REDIS_HOST 6379; do
-  log "Waiting for Redis..."
-  sleep 1
-done
-log "Redis is ready!"
-
-# Run database migrations
+# Apply database migrations
 log "Applying database migrations..."
 python manage.py migrate --noinput
 
@@ -29,6 +55,6 @@ python manage.py migrate --noinput
 log "Collecting static files..."
 python manage.py collectstatic --noinput
 
-# Start Gunicorn
-log "Starting Gunicorn..."
-exec "$@"
+# Start Django server
+log "Starting Django app..."
+exec python manage.py runserver 0.0.0.0:8000
