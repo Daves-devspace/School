f71ea53574af9a6ade58ad42aea2645993137380
David Maina <dmdeveloper255@gmail.com>
2025-02-16 19:01:15 +0300

    render

--- School/settings.py
+++ School/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = 'django-insecure-0yg9%d29(l9u^17*mm5=*%&jpev!x(&s(9q!ih12@i0a03zual'
 
 # # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True  # change to false in production
+DEBUG = False # change to false in production
 #
 ALLOWED_HOSTS = ['school-kbah.onrender.com', 'www.stitchngalore.com',
                  'localhost']  # Replace '*' with your domain or IP for production
@@ -138,16 +138,16 @@ except redis.exceptions.ConnectionError as e:
 
 #
 # # Initialize environment variables
-# env = environ.Env()
-# environ.Env.read_env()  # Read from .env (for local dev)
-#
-# DATABASE_URL = env.str("DATABASE_URL", default="")  # Read DATABASE_URL
-#
-# if DATABASE_URL:
-#     DATABASES = {"default": dj_database_url.config(default=DATABASE_URL)}
-# else:
-#     print("Warning: No DATABASE_URL set!")
-#     DATABASES = {}
+env = environ.Env()
+environ.Env.read_env()  # Read from .env (for local dev)
+
+DATABASE_URL = env.str("DATABASE_URL", default="")  # Read DATABASE_URL
+
+if DATABASE_URL:
+    DATABASES = {"default": dj_database_url.config(default=DATABASE_URL)}
+else:
+    print("Warning: No DATABASE_URL set!")
+    DATABASES = {}
 
 # # #celery test
 # DATABASES = {
@@ -156,18 +156,18 @@ except redis.exceptions.ConnectionError as e:
 
 
 
-
-# # # Database Configuration
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("DB_NAME"),
-        "USER": config("DB_USER"),
-        "PASSWORD": config("DB_PASSWORD"),
-        "HOST": config("DB_HOST", default="localhost"),
-        "PORT": config("DB_PORT", default="5432"),
-    }
-}
+#
+# # # # Database Configuration
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": config("DB_NAME"),
+#         "USER": config("DB_USER"),
+#         "PASSWORD": config("DB_PASSWORD"),
+#         "HOST": config("DB_HOST", default="localhost"),
+#         "PORT": config("DB_PORT", default="5432"),
+#     }
+# }
 
 # If using Celery
 CELERY_BROKER_URL = 'redis://redis:6379/1'
